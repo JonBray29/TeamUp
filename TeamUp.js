712351ca9bf210d5a33073c810e9983881d01d71
Jonathon Bray <jonathonbray@Jonathons-MacBook-Pro.local>
2020-11-13 14:02:28 +0000

    Added date and time

--- TeamUp.js
+++ TeamUp.js
@@ -1,3 +1,34 @@
 $(function(){
+    //Start the time
+    showTimeAndDate();
 
+    //Gets the current time and date and displays it, updates it every second.
+    function showTimeAndDate(){
+        let newDate = new Date();
+        let time = newDate.toLocaleTimeString();
+        let date = constructDate();
+        $("#time").html(time);
+        $("#date").html(date);
+        setTimeout(showTimeAndDate, 1000);
+    }
+    function constructDate(){
+        let newDate = new Date();
+        let weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        let month = ["January", "Febudary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+        let date = weekday[newDate.getDay() - 1] + " ";
+        date += newDate.getDate() + getOrdinal(newDate.getDate()) + " ";
+        date += month[newDate.getMonth()];
+
+        return date;
+    }
+    function getOrdinal(date){
+        if(date > 3 && date < 21) return "th";
+        switch (d % 10){
+            case 1: return "st";
+            case 2: return "nd";
+            case 3: return "rd";
+            default: return "th";
+        }
+    }
 })
