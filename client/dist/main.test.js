ce2fd9720b1ae5ab49cc369e64376b0c1149c809
Jon <jonbray29@hotmail.com>
2021-01-07 14:58:45 +0000

    remove after all

--- client/dist/main.test.js
+++ client/dist/main.test.js
@@ -6,9 +6,7 @@ var page;
 beforeAll(async () => {
     browser = await puppeteer.launch();
 });
-afterAll(async () => {
-    await browser.close();
-});
+
 beforeEach(async () => {
     page = await browser.newPage();
     await page.goto("https://elastic-mestorf-b1c185.netlify.app");
